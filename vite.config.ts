b530be254d425624a0347f021eecc2f95050fc6c
DeWorldR <hanrovth@gmail.com>
2025-08-31 18:31:16 +0700

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactlab01/', 
+  base: '/reactlab01/', // <-- ต้องตั้งตรงนี้
   plugins: [react()],
 })
